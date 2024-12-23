3e2aa8c10ddf2aaee3d1c9940c67fe3ea5a5028c
z0mbiebrad <bradleyt.evans@gmail.com>
2024-12-22 21:12:56 -0500

    Created task.create route to handle the new component

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Livewire\TaskCreate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('task-create', TaskCreate::class)
+        ->name('task.create');
 });
 
 require __DIR__.'/auth.php';
