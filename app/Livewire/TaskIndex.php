584c498984837b01e6802b98d70ea4b2afc6ebb2
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-03 17:34:04 -0500

    Added delete button

--- app/Livewire/TaskIndex.php
+++ app/Livewire/TaskIndex.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TaskIndex extends Component
 {
     public $tasks = '';
+
+    public function delete(Task $task)
+    {
+        $task->delete();
+        $this->tasks = Auth::user()->tasks;
+    }
 
     public function mount()
     {
