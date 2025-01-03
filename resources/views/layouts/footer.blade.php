82af9e15765d584e971993ae6281e2b857e132c9
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-03 17:35:39 -0500

    Got rid of the fixed footer as it was messing up scrolling. Addressed this issue in the project as i still need it to be at bottom of screen when theres no data.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-800 text-gray-400 py-4 mt-8 fixed bottom-0 w-full">
+<footer class="bg-gray-800 text-gray-400 py-4 mt-8">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex flex-col justify-between items-center">
             <!-- App Info -->
