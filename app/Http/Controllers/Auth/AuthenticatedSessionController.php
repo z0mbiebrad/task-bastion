952a14f9eee4fdf07cda7dfa76624e9ea777b05f
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-06 15:01:36 -0500

    changed route names

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('task.create', absolute: false));
+        return redirect()->intended(route('task.index', absolute: false));
     }
 
     /**
