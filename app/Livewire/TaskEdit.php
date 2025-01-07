d6ed0a4d652e8aa7d19a97cd173f0c1bfd9163c8
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-07 13:51:45 -0500

    Created my

---
+++ app/Livewire/TaskEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TaskEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.task-edit');
+    }
+}
