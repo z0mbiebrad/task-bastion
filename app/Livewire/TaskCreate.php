f8be9fe4116f57f1ba007d791c402bc814c79050
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-12 17:49:21 -0500

    Moved all form logic to the TaskForm class

--- app/Livewire/TaskCreate.php
+++ app/Livewire/TaskCreate.php
@@ -2,57 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\TaskForm;
 use App\Models\Task;
-use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\On;
-use Livewire\Attributes\Validate;
 use Livewire\Component;
-
-use function Laravel\Prompts\clear;
 
 class TaskCreate extends Component
 {   
-
-    #[Validate('required|string|min:3|max:255')]
-    public $task = '';
-
-    #[Validate('nullable|string|min:1|max:255')]
-    public $category = '';
-
-    #[Validate('nullable|string|min:1|max:255')]
-    public $customCategory = '';
-
-    #[Validate('nullable|integer|min:0|max:7')]
-    public $daysPerWeek = 0;
-
-    #[Validate('nullable|array')]
-    public $daysOfWeek = [];
-    
-    public $showFields = false;
-
-    public $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    public TaskForm $form;
 
     public function increment()
     {
-        $this->daysPerWeek++;
-        $this->updatedDaysPerWeek($this->daysPerWeek); 
+        $this->form->increment();
     }
  
     public function decrement()
     {
-        $this->daysPerWeek--;
-        $this->updatedDaysPerWeek($this->daysPerWeek); 
+        $this->form->decrement();
     }
 
     public function updatedDaysPerWeek($value)
     {
-        if ($value === 0) {
-            $this->daysOfWeek = [];
-        } elseif ($value < 0) {
-            $this->daysPerWeek = 0;
-        } elseif ($value > 7) {
-            $this->daysPerWeek = 7;
-        }
+        $this->form->updatedDaysPerWeek($value);
 
         $this->dispatch('updated-days');
     }
@@ -60,45 +31,21 @@ class TaskCreate extends Component
     #[On('updated-days')]
     public function updatedDaysOfWeek()
     {
-        // Dynamically limit daysOfWeek based on daysPerWeek
-        $maxDays = (int) $this->daysPerWeek;
-        if (count($this->daysOfWeek) > $maxDays) {
-            $this->daysOfWeek = array_slice($this->daysOfWeek, 0, $maxDays);
-            $this->addError('daysOfWeek', "You can only select up to {$maxDays} daasdasys.");
-        }
+        $this->form->updatedDaysOfWeek();
     }
 
     public function createTask()
     {
-        $this->validate();
-
-        $finalCategory = $this->category === 'custom' ? $this->customCategory : $this->category;
-
-        Task::create([
-            'user_id' => Auth::id(),
-            'task' => $this->task,
-            'category' => $finalCategory,
-            'daysPerWeek' => $this->daysPerWeek ?: 0,
-            'daysOfWeek' => $this->daysOfWeek,
+        $this->form->create();
+        $this->reset([
+            'form.task',
+            'form.category',
+            'form.daysOfWeek',
+            'form.daysPerWeek',
         ]);
-
-        $this->clearInput();
-
-        session()->flash('message', 'Task created successfully.');
-
         $this->dispatch('task-created');
     }
     
-    public function clearInput()
-    {
-        $this->showFields = false;
-        $this->task = '';
-        $this->category = '';
-        $this->customCategory = '';
-        $this->daysPerWeek = '';
-        $this->daysOfWeek = [];
-    }
-
     public function render()
     {
         return view('livewire.task-create');
