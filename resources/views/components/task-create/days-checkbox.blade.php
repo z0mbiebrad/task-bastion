6f7ba4557ad55e2abff8607e654df1e5ce0f5305
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-14 19:34:52 -0500

    solution for label issue

--- resources/views/components/task-create/days-checkbox.blade.php
+++ resources/views/components/task-create/days-checkbox.blade.php
@@ -1,12 +1,12 @@
 <div class="flex items-center">
     <input 
         type="checkbox" 
-        id="{{ $day }}" 
+        id="{{$formContext}}-{{ $day }}" 
         value="{{ $day }}" 
         wire:model.lazy="form.daysOfWeek" 
         class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out border-gray-300 dark:border-gray-600"
     >
-    <label for="{{ $day }}" class="ml-1 text-gray-900 dark:text-gray-200">
+    <label for="{{$formContext}}-{{ $day }}" class="ml-1 text-gray-900 dark:text-gray-200">
         {{ ucwords(substr($day,0,3)) }}
     </label>
 </div>
