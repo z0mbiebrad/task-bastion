787c4ae8c23b36f14e059d6ab6de82956fb950ce
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-19 11:54:13 -0500

    updated wire:model

--- resources/views/components/task-create/days-checkbox.blade.php
+++ resources/views/components/task-create/days-checkbox.blade.php
@@ -3,7 +3,7 @@
         type="checkbox" 
         id="{{$formContext}}-{{ $day }}" 
         value="{{ $day }}" 
-        wire:model.lazy="form.daysOfWeek" 
+        wire:model="form.customTaskDays" 
         class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out border-gray-300 dark:border-gray-600"
     >
     <label for="{{$formContext}}-{{ $day }}" class="ml-1 text-gray-900 dark:text-gray-200">
