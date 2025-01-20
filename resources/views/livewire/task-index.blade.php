24604bdac70c5d9f79fc454ff430d17b3f0493bf
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-20 07:57:12 -0500

    Logic for up down edit arrow

--- resources/views/livewire/task-index.blade.php
+++ resources/views/livewire/task-index.blade.php
@@ -71,15 +71,26 @@
                             </svg>
                         </button>
                         <button 
-                            class="text-blue-500 dark:text-blue-400"
+                            class="text-blue-500 dark:text-blue-400 flex"
                             wire:click="toggleEdit({{ $task->id }})"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                             </svg>
+                            @isset($editing[$task->id])
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                            </svg>
+                            @else
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                            </svg>
+                            @endisset
                         </button>
+                      
                     </div>
                 </div>
+            
             </label>
             @if (isset($editing[$task->id]))
             <livewire:task-edit :task="$task" :key="$task->id"></livewire:task-edit>
