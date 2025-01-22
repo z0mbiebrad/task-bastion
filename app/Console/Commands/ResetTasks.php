5f5835f5bb138f5a2eb08b611a7edf29e5efe310
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-22 18:19:50 -0500

    The command that holds the logic for resetting the tasks

---
+++ app/Console/Commands/ResetTasks.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Task;
+use Illuminate\Console\Command;
+
+class ResetTasks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reset-tasks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Resets the users tasks every night';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Task::query()->update(['completed' => false]);
+        $this->info('All tasks have been reset successfully');
+    }
+}
