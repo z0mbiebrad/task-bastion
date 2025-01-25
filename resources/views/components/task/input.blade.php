1d36f1551c4857f32187e5b7a56dd3c760b33f1b
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-25 18:02:43 -0500

    refactor

--- resources/views/components/task/input.blade.php
+++ resources/views/components/task/input.blade.php
@@ -1,16 +1,21 @@
 <div class="mb-4">
-    <label
+    <div class="flex items-center justify-between">
+        <label
         x-show="showFields"
         x-transition:enter.duration.500ms
         x-transition:leave.duration.0ms
         for="task" 
         class="block text-lg font-medium text-gray-700 dark:text-gray-300 mb-2"
-    >
-        Describe your non-negotiable
-        <span class="block text-gray-500 dark:text-gray-400 text-sm italic mt-1">
-            e.g., "Read for 30 minutes" or "Meditate for 10 minutes"
-        </span>
-    </label>
+        >
+            Describe your task
+            <span class="block text-gray-500 dark:text-gray-400 text-sm italic mt-1">
+                e.g., "Read for 30 minutes" or "Meditate for 10 minutes"
+            </span>
+        </label>
+        <!-- Submit Button -->
+        <x-task.submit-button />
+    </div>
+    
     <input 
         wire:model.blur='form.task' 
         x-on:input="
@@ -23,7 +28,7 @@
         type="text" 
         id="task"
         class="mt-1 block w-full px-3 py-2 bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-200 border dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 placeholder-gray-100 @error('form.task') required:border-red-500 @enderror"
-        placeholder="Add a non-negotiable..." 
+        placeholder="Add a task" 
         required 
     />
 </div>
