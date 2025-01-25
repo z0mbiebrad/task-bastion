81b813e23cb1fa79c00a36410095ea7cb26d8353
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-25 17:59:25 -0500

    Created a navigation livewire component to incorporate my "edit tasks" button

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,19 +9,21 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxStyles
     </head>
     <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
         <!-- Navigation -->
-        @include('layouts.navigation')
+        <livewire:navigation></livewire:navigation>
         
         <!-- Page Heading -->
         @isset($header)
             <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-3xl mx-auto py-6 px-4">
                     {{ $header }}
                 </div>
             </header>
@@ -34,5 +36,6 @@
         
         <!-- Footer -->
         @include('layouts.footer')
+        @fluxScripts
     </body>
 </html>
