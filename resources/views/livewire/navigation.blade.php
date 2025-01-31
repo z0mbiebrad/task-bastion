f3ee3bf5cfd6f7d6fb1bb620c77a76c4d02d90d5
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-31 07:05:38 -0500

    Toggle-edi was confusing and not accurate, changed to edit-toggle

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -28,7 +28,7 @@
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
                     <button
                         class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
-                        wire:click="$dispatch('toggle-Edi')"
+                        wire:click="$dispatch('edit-toggle')"
                     >
                         <div>
                             Edit Tasks
