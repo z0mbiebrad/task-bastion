283cb0f58265ad35360bdb3a5f8410f4f5bdea9d
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-08 22:41:51 -0500

    Darkmode toggle with alpine

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,11 +11,22 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <style>
+            /* Smooth transition */
+            * { transition: background-color 0.3s, color 0.3s; }
+        </style>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased bg-white dark:bg-black">
+    <body 
+        x-data="{ 
+            darkMode: localStorage.theme === 'dark' || (!localStorage.theme && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+        }" 
+        x-init="$watch('darkMode', val => localStorage.theme = val ? 'dark' : 'light')" 
+        :class="{ 'dark': darkMode }"
+        class="font-sans antialiased bg-white dark:bg-black"
+    >
+
         <!-- Page Content -->
         <main class="min-h-screen">
             <livewire:navigation/>
