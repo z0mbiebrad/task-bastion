e2e2a8169ecbf711c85c5c76865a1d48cd3ee35f
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-10 12:02:16 -0500

    refactor: update task form layout for improved structure and styling

--- resources/views/components/task/form.blade.php
+++ resources/views/components/task/form.blade.php
@@ -11,44 +11,33 @@
 >
     <form
 
-        class="p-4 border-b border-neutral-300 dark:border-neutral-700"
+        class="px-6 py-4 border-b max-w-2xl mx-auto border-neutral-300 dark:border-neutral-700"
         wire:submit.prevent="{{ $submitAction }}"
     >
-            {{-- Task Input --}}
             <x-task.input
                 :formContext="$formContext"
             />
 
-            {{-- Task Input Error --}}
             <x-task.error-message type="form.task"/>
 
             <div
                 x-show="showFields"
             >
                 
-                {{-- Category Input --}}
-                <x-task.category-input />
+                <div class="flex justify-between mx-auto gap-4">
+                    <x-task.category-input />
 
-                {{-- Category Input Error --}}
-                <x-task.error-message type="form.category"/>
-
-                {{-- Custom Category --}}
+                    <x-task.days-a-week
+                        :taskDays="$form->taskDays"
+                    />
+                </div>
                 <x-task.custom-category />
-
-                {{-- Custom Category Error --}}
-                <x-task.error-message type="form.customCategory"/>
-
-
-                {{-- Days a week Input --}}
-                <x-task.days-a-week
-                    :days="$form->days"
-                    :taskDays="$form->taskDays"
+                <x-task.custom-days 
                     :formContext="$formContext"
                 />
-
-                <x-task.error-message type="form.taskDays"/>
-
+                <x-task.error-message type="form.customCategory"/>
                 <x-task.error-message type="form.customTaskDays"/>
             </div>
+            <x-task.submit-button :formContext="$formContext" />
     </form>
 </div>
