fc594d04651c3083367090eef0e2d3b35c54f1ee
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-10 12:02:06 -0500

    fix: update label display for days checkbox to show single character

--- resources/views/components/task/days-checkbox.blade.php
+++ resources/views/components/task/days-checkbox.blade.php
@@ -7,6 +7,6 @@
         class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out border-gray-300 dark:border-gray-600"
     >
     <label for="{{$formContext}}-{{ $day }}" class="ml-1 text-gray-900 dark:text-gray-200">
-        {{ ucwords(substr($day,0,3)) }}
+        {{ ucwords(substr($day,0,1)) }}
     </label>
 </div>
