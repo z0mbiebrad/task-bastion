f127e08007ae5c0a31ab7fe972a9d16bbec608c0
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-13 19:21:30 -0500

    Refactored how tasks are assigned

--- app/Livewire/TaskIndex.php
+++ app/Livewire/TaskIndex.php
@@ -3,24 +3,22 @@
 namespace App\Livewire;
 
 use App\Traits\HandlesTasks;
-use App\Traits\HandlesGuestTasks;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
-use Livewire\Attributes\Session;
 use Livewire\Component;
 
 class TaskIndex extends Component
 {
     use HandlesTasks;
-    use HandlesGuestTasks;
 
     public array|Collection $tasks = [];
 
     public string $guest_id = '';
     public array $editing = [];
     public $toggleEditButton = false;
+    
     #[Computed]
     public function groupedTasks()
     {
@@ -29,18 +27,12 @@ class TaskIndex extends Component
         });
     }
 
-    #[On(['task-created', 'task-deleted', 'task-completed'])]
-    public function determineUser()
-    {
-        auth()->check() ? $this->loadTasks() : $this->loadGuestTasks();
-    }
-
     public function mount()
     {
         if (!session()->has('guest_id')) {
             session(['guest_id' => Str::uuid()->toString()]);
         }
-        $this->determineUser();
+        $this->loadTasks();
     }
 
     #[On('edit-toggle')]
