ab36dcc581d9e87e592fc98e6ba74f01b8a560b6
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-13 19:23:17 -0500

    Changed auth() to Auth:: for readability and intellisense

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -66,7 +66,7 @@
         </button>
         <!-- Mobile Menu -->
         <ul x-cloak x-show="mobileMenuIsOpen" x-transition:enter="transition motion-reduce:transition-none ease-out duration-300" x-transition:enter-start="-translate-y-full" x-transition:enter-end="translate-y-0" x-transition:leave="transition motion-reduce:transition-none ease-out duration-300" x-transition:leave-start="translate-y-0" x-transition:leave-end="-translate-y-full" id="mobileMenu" class="fixed max-h-svh overflow-y-auto inset-x-0 top-0 z-10 flex flex-col divide-y divide-neutral-300 rounded-b-sm border-b border-neutral-300 bg-neutral-50 px-6 pb-6 pt-20 dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-900 md:hidden">
-            @if (!auth()->check())
+            @if (!Auth::check())
                 <li class="py-4">
                     <a href="{{ route('login') }}" class="w-full text-lg font-medium text-neutral-600 focus:underline dark:text-neutral-300">
                         Login
