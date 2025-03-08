4764e7ee3ec3831bf605a1120094de9061e82b1b
z0mbiebrad <bradleyt.evans@gmail.com>
2025-03-08 10:16:45 -0500

    wip

--- resources/views/components/category-header.blade.php
+++ resources/views/components/category-header.blade.php
@@ -1,6 +1,6 @@
     <div class="my-8">
         @if ($category)
-            <span class="flex items-center max-w-2xl mx-auto">
+            <span class="flex items-center max-w-3xl mx-auto">
                 <span class="h-px flex-1 bg-neutral-700 dark:bg-neutral-200"></span>
                 <span class="shrink-0 px-6 text-2xl dark:text-neutral-200">
                     {{ ucwords($category) }}
@@ -10,7 +10,7 @@
         @endif
 
         @if (!$category)
-            <span class="flex items-center max-w-2xl mx-auto">
+            <span class="flex items-center max-w-3xl mx-auto">
                 <span class="h-px flex-1 bg-neutral-700 dark:bg-neutral-200"></span>
                 <span class="shrink-0 px-6 text-2xl dark:text-neutral-200">
                     Uncategorized
